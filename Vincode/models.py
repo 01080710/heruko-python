16ecf7644890736520908a9d6f32d848b37ba87d
88696 <zxcvbnm951654@kimo.com>
2023-12-31 14:56:55 +0800

    make it better

--- Vincode/models.py
+++ Vincode/models.py
@@ -15,7 +15,7 @@ class Item(models.Model):
     def __str__(self):
         return self.text
     
-class CombineLexus(models.Model):
+class hotai(models.Model):
     
     類型 = models.CharField(("類型"),max_length=50, null=True)  
     車種 = models.CharField("車種",max_length=50, null=True)  
@@ -25,7 +25,8 @@ class CombineLexus(models.Model):
     年份 = models.CharField("年份",max_length=50, null=True)  
     WMI = models.CharField("WMI",max_length=50, null=True)  
     VDS = models.CharField("VDS",max_length=50, null=True)  
-    VALADATION = models.CharField("VALADATION",max_length=50, null=True)  
+    VALADATION = models.CharField("VALADATION",max_length=50, null=True) 
+    TOYOTA進口_VALADATION = models.CharField("TOYOTA進口_VALADATION",max_length=50, null=True)
     VIS_min = models.CharField("VIS_min",max_length=50, null=True) 
     VIS_max = models.CharField("VIS_max",max_length=50, null=True)
     match = models.CharField("match",max_length=50, null=True) 
@@ -70,75 +71,25 @@ class CombineLexus(models.Model):
     左後座腿靠氣囊 = models.CharField("左後座腿靠氣囊",max_length=50, null=True)  
     右後座防潛滑氣囊 = models.CharField("右後座防潛滑氣囊",max_length=50, null=True)  
     右後座腿靠氣囊 = models.CharField("右後座腿靠氣囊",max_length=50, null=True) 
-    
+    VIS_min_false = models.CharField("VIS_min_false",max_length=50, null=True) 
+    VIS_max_false = models.CharField("VIS_max_false",max_length=50, null=True) 
+      
     def __str__(self):
         return self.name
     
+
+    
     # 使用實際存在的欄位名稱
     class Meta:
-        ordering = ['類型', '車種', 'CARMDL', 'SFX','出廠年月','年份','WMI','VDS','VALADATION','VIS_min','VIS_max','match','車身碼','中文等級',
+        ordering = ['類型', '車種', 'CARMDL', 'SFX','出廠年月','年份','WMI','VDS','VALADATION','TOYOTA進口_VALADATION','VIS_min','VIS_max','match','車身碼','中文等級',
                     '排氣量','價格','估價日期','保險到期日','銷售日','統計代碼','限乘人數','Label','車身座位','變速系統','動力型式','車身型式','車重' ,                   
                     '定速巡航','蜂鳴式倒車輔助系統','倒車影像輔助','前方駐車雷達','循跡防滑控制系統','車道偏離警示系統','車道變換輔助系統','ACC主動跟車系統',
                     '車道維持輔助系統','AEB自動緊急煞車系統','電子煞車力分配系統','EBS','雷達碰撞預防系統','自動停車系統','雙前座正面氣囊','雙前座側面氣囊',
                     '前座中央防碰撞氣囊','右前座膝部氣囊','右前座正面氣囊','後座側面氣囊','駕駛座正面氣囊','駕駛座膝部氣囊','雙前座膝部氣囊','左後座腿靠氣囊',
-                    '右後座防潛滑氣囊','右後座腿靠氣囊']
+                    '右後座防潛滑氣囊','右後座腿靠氣囊','VIS_min_false','VIS_max_false']
 
 
-# class CombineLexus(models.Model):
-    
-#     類型 = models.CharField(("類型"),max_length=50, null=True)  
-#     車種 = models.CharField("車種",max_length=50, null=True)  
-#     VIN = models.CharField("VIN",max_length=50, null=True)  
-#     中文等級 = models.CharField("中文等級",max_length=50, null=True)  
-#     出廠年月 = models.CharField("出廠年月",max_length=50, null=True)  
-#     排氣量_x = models.CharField("排氣量_x",max_length=50, null=True)  
-#     車身型式 = models.CharField("車身型式",max_length=50, null=True)  
-#     車重 = models.CharField("車重",max_length=50, null=True)  
-#     價格_x = models.CharField("價格_x",max_length=50, null=True)  
-#     車牌號碼 = models.CharField("車牌號碼",max_length=50, null=True) 
-#     CARMDL = models.CharField("CARMDL",max_length=50, null=True)
-#     SFX = models.CharField("SFX",max_length=50, null=True) 
-#     價格_y = models.CharField("價格_y",max_length=50, null=True)  
-#     車身座位 = models.CharField("車身座位",max_length=50, null=True)  
-#     變速系統 = models.CharField("變速系統",max_length=50, null=True)  
-#     動力型式 = models.CharField("動力型式",max_length=50, null=True)  
-#     定速巡航 = models.CharField("定速巡航",max_length=50, null=True)  
-#     蜂鳴式倒車輔助系統 = models.CharField("蜂鳴式倒車輔助系統",max_length=50, null=True)  
-#     倒車影像輔助 = models.CharField("倒車影像輔助",max_length=50, null=True)  
-#     前方駐車雷達 = models.CharField("前方駐車雷達",max_length=50, null=True)  
-#     循跡防滑控制系統 = models.CharField("循跡防滑控制系統",max_length=50, null=True)  
-#     車道偏離警示系統 = models.CharField("車道偏離警示系統",max_length=50, null=True)  
-#     車道變換輔助系統 = models.CharField("車道變換輔助系統",max_length=50, null=True)  
-#     ACC主動跟車系統 = models.CharField("ACC主動跟車系統",max_length=50, null=True)  
-#     車道維持輔助系統 = models.CharField("車道維持輔助系統",max_length=50, null=True)  
-#     AEB自動緊急煞車系統 = models.CharField("AEB自動緊急煞車系統",max_length=50, null=True)  
-#     電子煞車力分配系統 = models.CharField("電子煞車力分配系統",max_length=50, null=True)  
-#     EBS = models.CharField("EBS",max_length=50, null=True)  
-#     雷達碰撞預防系統 = models.CharField("雷達碰撞預防系統",max_length=50, null=True)  
-#     自動停車系統 = models.CharField("自動停車系統",max_length=50, null=True)  
-#     雙前座正面氣囊 = models.CharField("雙前座正面氣囊",max_length=50, null=True)  
-#     雙前座側面氣囊 = models.CharField("雙前座側面氣囊",max_length=50, null=True)  
-#     前座中央防碰撞氣囊 = models.CharField("前座中央防碰撞氣囊",max_length=50, null=True)  
-#     右前座膝部氣囊 = models.CharField("右前座膝部氣囊",max_length=50, null=True)  
-#     右前座正面氣囊 = models.CharField("右前座正面氣囊",max_length=50, null=True)  
-#     後座側面氣囊 = models.CharField("後座側面氣囊",max_length=50, null=True)  
-#     駕駛座正面氣囊 = models.CharField("駕駛座正面氣囊",max_length=50, null=True) 
-#     駕駛座膝部氣囊 = models.CharField("駕駛座膝部氣囊",max_length=50, null=True)  
-#     雙前座膝部氣囊 = models.CharField("雙前座膝部氣囊",max_length=50, null=True)  
-#     左後座腿靠氣囊 = models.CharField("左後座腿靠氣囊",max_length=50, null=True)  
-#     右後座防潛滑氣囊 = models.CharField("右後座防潛滑氣囊",max_length=50, null=True)  
-#     右後座腿靠氣囊 = models.CharField("右後座腿靠氣囊",max_length=50, null=True) 
-    
-#     def __str__(self):
-#         return self.name
-    
-#     # 使用實際存在的欄位名稱
-#     class Meta:
-#         ordering = ['類型', '車種', 'VIN', '中文等級','出廠年月','排氣量_x','車身型式','車重','價格_x','車牌號碼','CARMDL','SFX','價格_y','車身座位',
-#                     '變速系統','動力型式','定速巡航','蜂鳴式倒車輔助系統','倒車影像輔助','前方駐車雷達','循跡防滑控制系統','車道偏離警示系統',
-#                     '車道變換輔助系統','ACC主動跟車系統','車道維持輔助系統','AEB自動緊急煞車系統','電子煞車力分配系統','EBS','雷達碰撞預防系統',
-#                     '自動停車系統','雙前座正面氣囊','雙前座側面氣囊','前座中央防碰撞氣囊','右前座膝部氣囊','右前座正面氣囊','後座側面氣囊','駕駛座正面氣囊',
-#                     '駕駛座膝部氣囊','雙前座膝部氣囊','左後座腿靠氣囊','右後座防潛滑氣囊','右後座腿靠氣囊']
+
 
 
 
